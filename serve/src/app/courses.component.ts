1d93f322db2e89e924bf8f9023056c9daafcb878
Daniel Ball <g.thirteen333@gmail.com>
2020-01-27 10:37:56 -0500

    boot strap added and courses render successfully

--- serve/src/app/courses.component.ts
+++ serve/src/app/courses.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
+import {CoursesService} from './courses.service';
 
 @Component({// getTitle() can be used also
     selector: 'courses',
     template: `
-         <h2>{{ "Title:" + title }}</h2> 
+         <h2>{{ title }}</h2> 
         <ul>
             <li *ngFor="let course of courses">
                 {{ course }}
             </li>
         </ul>
+        <button class="btn btn-primary">Press Button</button>
     ` 
 })
 export class CoursesComponent {
     title = "List of courses";
-    courses = ["course1", "course2", "course3", "course4"];
+    courses;
+
+    constructor(service: CoursesService) {
+        this.courses = service.getCourses();
+    }
 
     getTitle() {
         return this.title;
